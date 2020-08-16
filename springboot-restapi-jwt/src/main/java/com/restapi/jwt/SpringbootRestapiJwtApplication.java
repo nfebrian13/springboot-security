6c1051fc4b00f068154608dc9e01e22d85db8888
nfebrian13 <nanaz.febrian@gmail.com>
2020-08-16 12:31:24 +0700

    initial commit

---
+++ springboot-restapi-jwt/src/main/java/com/restapi/jwt/SpringbootRestapiJwtApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiJwtApplication.class, args);
+	}
+
+}
